270392d3f7771b04305d58a19e4345085f4b3d80
Saransh Kumar <lunaticdevs1337@gmail.com>
2024-10-20 12:47:53 +0530

    Use relative path short-hand

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
-import PlayView from "../views/PlayView.vue"
-import ErrorView from "../views/ErrorView.vue"
+import HomeView from "@/views/HomeView.vue"
+import PlayView from "@/views/PlayView.vue"
+import ErrorView from "@/views/ErrorView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

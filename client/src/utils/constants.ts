b8715d3d9a93fb27bb137b6f5964393f52095d3e
Saransh Kumar <lunaticdevs1337@gmail.com>
2024-12-06 17:40:27 +0530

    Decrease pingCooldown to 15

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -5,7 +5,7 @@ export const Constants = {
   maxTurnRate: 0.03,
   snakeSegmentDistance: 15,
   snakeSegmentDiameter: 50,
-  pingCooldown: 60,
+  pingCooldown: 15,
   worldBoundary: {
     radius: 2750,
     minX: -3000,

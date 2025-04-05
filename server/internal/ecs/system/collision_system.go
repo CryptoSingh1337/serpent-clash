ecccf70ce5dff91a70e93475de8a8d9816e5a374
Saransh Kumar <lunaticdevs1337@gmail.com>
2025-04-06 00:02:54 +0530

    Implement collision with world boundary

--- server/internal/ecs/system/collision_system.go
+++ server/internal/ecs/system/collision_system.go
@@ -1,6 +1,10 @@
 package system
 
-import "github.com/CryptoSingh1337/serpent-clash/server/internal/ecs/storage"
+import (
+	"github.com/CryptoSingh1337/serpent-clash/server/internal/ecs/component"
+	"github.com/CryptoSingh1337/serpent-clash/server/internal/ecs/storage"
+	"github.com/CryptoSingh1337/serpent-clash/server/internal/utils"
+)
 
 type CollisionSystem struct {
 	storage storage.Storage
@@ -13,6 +17,28 @@ func NewCollisionSystem(storage storage.Storage) *CollisionSystem {
 }
 
 func (c *CollisionSystem) Update() {
+	// Get all players
+	playerEntities := c.storage.GetAllEntitiesByType(utils.PlayerEntity)
+	for _, playerId := range playerEntities {
+		comp := c.storage.GetComponentByEntityIdAndName(playerId, utils.SnakeComponent)
+		if comp == nil {
+			continue
+		}
+		snakeComponent := comp.(*component.Snake)
+		head := snakeComponent.Segments[0]
+		distanceFromOrigin := utils.EuclideanDistance(0, 0, head.X, head.Y)
+		if distanceFromOrigin >= utils.WorldBoundaryRadius {
+			comp = c.storage.GetComponentByEntityIdAndName(playerId, utils.NetworkComponent)
+			if comp == nil {
+				continue
+			}
+			networkComponent := comp.(*component.Network)
+			if err := networkComponent.Connection.Close(); err != nil {
+				utils.Logger.Err(err).Msgf("error while closing connection for player")
+			}
+		}
+
+	}
 }
 
 func (c *CollisionSystem) Stop() {

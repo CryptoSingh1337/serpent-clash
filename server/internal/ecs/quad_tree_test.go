07a49a525ce74067c5f539d51e51a20044d7533a
Saransh Kumar <lunaticdevs1337@gmail.com>
2025-04-07 23:44:22 +0530

    Completed implementation of QuadTree

--- server/internal/ecs/quad_tree_test.go
+++ server/internal/ecs/quad_tree_test.go
@@ -7,9 +7,23 @@ import (
 )
 
 func TestQuadTree_Insert(t *testing.T) {
-	qt := NewQuadTree(Rect{X: 200, Y: 200, W: 200, H: 200}, 4)
-	for i := 1; i <= 5; i++ {
+	qt := NewQuadTree(BBox{X: 200, Y: 200, W: 200, H: 200}, 4)
+	for i := 1; i <= 10; i++ {
 		qt.Insert(Point{X: rand.Float64() * qt.Boundary.W, Y: rand.Float64() * qt.Boundary.H})
-		fmt.Printf("QT after %v insert: %v\n", i, qt)
+	}
+	qt.Print()
+}
+
+func TestQuadTree_QueryBCircle(t *testing.T) {
+	qt := NewQuadTree(BBox{X: 200, Y: 200, W: 200, H: 200}, 4)
+	for i := 1; i <= 1000; i++ {
+		qt.Insert(Point{X: rand.Float64() * qt.Boundary.W, Y: rand.Float64() * qt.Boundary.H})
+	}
+	qt.Print()
+
+	var points []Point
+	qt.QueryBCircle(BCircle{X: 150, Y: 150, R: 10}, &points)
+	for _, point := range points {
+		fmt.Println(point)
 	}
 }

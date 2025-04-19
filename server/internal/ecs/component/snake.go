2db1b3ef0250275e95cfc13e66a9210baca21797
Saransh Kumar <lunaticdevs1337@gmail.com>
2025-04-20 00:42:07 +0530

    Refactor code

--- server/internal/ecs/component/snake.go
+++ server/internal/ecs/component/snake.go
@@ -3,7 +3,6 @@ package component
 import (
 	"fmt"
 	"github.com/CryptoSingh1337/serpent-clash/server/internal/utils"
-	"math"
 	"math/rand"
 )
 
@@ -20,23 +19,4 @@ func NewSnakeComponent() Snake {
 		IsAlive: true,
 		Angle:   0,
 	}
-}
-
-func generateRandomPosition(length int) []utils.Coordinate {
-	totalSnakeLength := float64((length - 1) * utils.SnakeSegmentDistance)
-	maxRadius := utils.WorldBoundaryRadius - totalSnakeLength
-
-	theta := rand.Float64() * 2 * math.Pi
-	radius := rand.Float64() * maxRadius
-
-	x := radius * math.Cos(theta)
-	y := radius * math.Sin(theta)
-
-	segments := make([]utils.Coordinate, length)
-	segments[0] = utils.Coordinate{X: x, Y: y}
-	for i := 1; i < len(segments); i++ {
-		segments[i].X = x - float64(i)*utils.SnakeSegmentDistance*math.Cos(theta)
-		segments[i].Y = y - float64(i)*utils.SnakeSegmentDistance*math.Sin(theta)
-	}
-	return segments
 }

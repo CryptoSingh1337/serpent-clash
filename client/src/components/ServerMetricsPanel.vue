41d2473c73217e2ea1b1bb8eab441c604ca77e39
Saransh Kumar <lunaticdevs1337@gmail.com>
2025-06-04 23:40:07 +0530

    Reformat code

--- client/src/components/ServerMetricsPanel.vue
+++ client/src/components/ServerMetricsPanel.vue
@@ -11,12 +11,46 @@ defineProps<{
 <template>
   <div class="w-full">
     <div class="flex flex-wrap justify-center gap-4 mb-4">
-      <ServerPanel label="CPU Usage" :value="serverMetrics.cpuUsage" suffix="%" icon="bi bi-pc-horizontal" color="text-green-500" />
-      <ServerPanel label="Memory Usage" :value="bytesToMB(serverMetrics.memoryUsage)" suffix="MB" icon="bi bi-memory" color="text-yellow-400" />
-      <ServerPanel label="No. of Goroutines" :value="serverMetrics.numGoroutines" icon="bi bi-cpu" color="text-indigo-400" />
-      <ServerPanel label="Heap objects" :value="serverMetrics.heapObjects" icon="bi bi-circle-fill" color="text-fuchsia-400" />
-      <ServerPanel label="Last GC run" :value="serverMetrics.lastGCMs" suffix="ms ago" icon="bi bi-circle-fill" color="text-orange-400" />
-      <ServerPanel label="Last GC Pause" :value="serverMetrics.gcPauseMicro" suffix="μs ago" icon="bi bi-circle-fill" color="text-rose-400" />
+      <ServerPanel
+        label="CPU Usage"
+        :value="serverMetrics.cpuUsage"
+        suffix="%"
+        icon="bi bi-pc-horizontal"
+        color="text-green-500"
+      />
+      <ServerPanel
+        label="Memory Usage"
+        :value="bytesToMB(serverMetrics.memoryUsage)"
+        suffix="MB"
+        icon="bi bi-memory"
+        color="text-yellow-400"
+      />
+      <ServerPanel
+        label="No. of Goroutines"
+        :value="serverMetrics.numGoroutines"
+        icon="bi bi-cpu"
+        color="text-indigo-400"
+      />
+      <ServerPanel
+        label="Heap objects"
+        :value="serverMetrics.heapObjects"
+        icon="bi bi-circle-fill"
+        color="text-fuchsia-400"
+      />
+      <ServerPanel
+        label="Last GC run"
+        :value="serverMetrics.lastGCMs"
+        suffix="ms ago"
+        icon="bi bi-circle-fill"
+        color="text-orange-400"
+      />
+      <ServerPanel
+        label="Last GC Pause"
+        :value="serverMetrics.gcPauseMicro"
+        suffix="μs ago"
+        icon="bi bi-circle-fill"
+        color="text-rose-400"
+      />
     </div>
   </div>
 </template>
--- client/src/components/server/Panel.vue
+++ client/src/components/server/Panel.vue
@@ -10,7 +10,7 @@ defineProps<{
 
 <template>
   <div
-      class="bg-gray-700 rounded-lg p-4 text-center min-w-[160px] flex-1 max-w-[240px] shadow-md border border-gray-600 hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1"
+    class="bg-gray-700 rounded-lg p-4 text-center min-w-[160px] flex-1 max-w-[240px] shadow-md border border-gray-600 hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1"
   >
     <div class="text-xl font-bold mb-2 text-gray-200">
       <i :class="[icon, color]" class="mr-2"></i>
